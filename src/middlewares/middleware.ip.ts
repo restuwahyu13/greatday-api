371f6c22ec7afe2c9fc6b6e9fa67b9179c5e6d8e
restuwahyu13 <restuwahyu13@gmail.com>
2024-02-10 22:41:53 +0700

    Remove req headers in middleware ip

--- src/middlewares/middleware.ip.ts
+++ src/middlewares/middleware.ip.ts
@@ -2,7 +2,7 @@ import { Handler, NextFunction, Request, Response } from 'express'
 import { randomDomainName, randomIpAddress } from '~/helpers/helper.randomString'
 
 export const ip = (): Handler => {
-	return (req: Request, res: Response, next: NextFunction): void => {
+	return (_req: Request, res: Response, next: NextFunction): void => {
 		const ip: string = randomIpAddress()
 		const domain: string = randomDomainName()
 
@@ -13,14 +13,6 @@ export const ip = (): Handler => {
 		res.setHeader('X-Forwarded-Host', domain)
 		res.setHeader('X-Forwarded-For', ip)
 		res.setHeader('X-Real-Ip', ip)
-
-		req.headers['host'] = domain
-		req.headers['origin'] = domain
-		req.headers['referer'] = domain
-
-		req.headers['X-Forwarded-Host'] = domain
-		req.headers['X-Forwarded-For'] = ip
-		req.headers['X-Real-Ip'] = ip
 
 		next()
 	}
