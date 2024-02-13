981014d45f6a4c2c04aa536dd60bb187022e03a6
restuwahyu13 <restuwahyu13@gmail.com>
2024-02-13 21:08:24 +0700

    Fix fileupload error

--- src/libs/lib.multer.ts
+++ src/libs/lib.multer.ts
@@ -1,23 +1,16 @@
 import { Request } from 'express'
 import multer from 'multer'
-import { FileTypeResult, fromFile } from 'file-type'
 
 import { ConfigsEnvironment } from '~/configs/config.env'
 import { Injectable } from '~/helpers/helper.di'
 
 @Injectable()
 export class Multer {
-	private async fileFilter(req: Request, file: Express.Multer.File, done: (error: Error, destination: string) => void): Promise<void> {
-		const fileType: FileTypeResult = await fromFile(file.originalname)
-
+	private fileFilter(req: Request, file: Express.Multer.File, done: (error: Error, destination: string) => void): void {
 		if (!req.header('content-type').includes('multipart/form-data')) {
 			done(new Error('Content-Type is not valid'), null)
 		} else if (+req.header('content-length') >= ConfigsEnvironment.FILE_SIZE_MAX) {
 			done(new Error('File to many large'), null)
-		} else if (!fileType) {
-			done(new Error('Invalid file type'), null)
-		} else if (ConfigsEnvironment.FILE_MIME_TYPE.split(',').indexOf(fileType.ext) === -1) {
-			done(new Error('Invalid mime type'), null)
 		} else {
 			done(null, file.originalname)
 		}
--- src/middlewares/middleware.upload.ts
+++ src/middlewares/middleware.upload.ts
@@ -2,12 +2,13 @@ import { Request, Response, NextFunction, Handler } from 'express'
 import { StatusCodes as status } from 'http-status-codes'
 import { Multer } from 'multer'
 import { OutgoingMessage } from 'http'
+import { FileTypeResult, fromBuffer } from 'file-type'
 
 import { Inject, Middleware } from '~/helpers/helper.di'
-import { MimeType } from '~/helpers/helper.mimeType'
 import { apiResponse } from '~/helpers/helper.apiResponse'
 import { fileStream } from '~/helpers/helper.fileStream'
 import { UsersMetadata } from '~/helpers/helper.usersMetadata'
+import { ConfigsEnvironment } from '~/configs/config.env'
 
 @Middleware()
 export class FilesMiddleware {
@@ -23,13 +24,17 @@ export class FilesMiddleware {
 			fileupload(req, res, async <Error>(err: Error): Promise<NextFunction | OutgoingMessage> => {
 				try {
 					if (err instanceof Error) {
-						throw apiResponse({ err_message: err })
+						throw apiResponse({ stat_code: status.UNPROCESSABLE_ENTITY, err_message: err.message })
 					}
 
-					if (!req.file) {
-						throw apiResponse({ stat_code: status.UNPROCESSABLE_ENTITY, err_message: 'Invalid file upload' })
-					} else if (!MimeType.whiteListBytes(req.file.buffer, req.file.originalname)) {
+					const fileType: FileTypeResult = await fromBuffer(req.file.buffer)
+
+					if (!fileType) {
+						throw apiResponse({ stat_code: status.UNPROCESSABLE_ENTITY, err_message: 'Invalid file type' })
+					} else if (ConfigsEnvironment.FILE_MIME_TYPE.split(',').indexOf(fileType.ext) === -1) {
 						throw apiResponse({ stat_code: status.UNPROCESSABLE_ENTITY, err_message: 'Invalid mime type' })
+					} else if (req.file.size >= ConfigsEnvironment.FILE_SIZE_MAX) {
+						throw apiResponse({ stat_code: status.UNPROCESSABLE_ENTITY, err_message: 'File to many large' })
 					}
 
 					req.file.filename = await fileStream(req.file, this.usersMetadata.user())
